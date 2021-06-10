8e48bb69d079db75fa2825fd8f5b97259d373fb8
Ahdini Zahra <78951804+Ahdini@users.noreply.github.com>
2021-06-11 00:09:28 +0700

    Create

---
+++ profil.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <link rel="stylesheet" href="profil.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+</head>
+<body>
+    <!-- ======= Header ======= -->
+    <div class="banner">
+        <div class="navbar order-last order-lg-0">
+            <h1>RASA</h1>
+            <ul>
+                <li><a class="nav-link scrollto active" href="after.php">Beranda</a></li>
+                  <li class="dropdown"><a href="#"><span>Reservasi</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                      <li><a href="buat-reservasi.php">Buat Reservasi</a></li>
+                      <li><a href="reservasi-saya.php">Reservasi Saya</a></li>
+                      <li><a href="riwayat-reservasi.php">Riwayat Reservasi</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a href="#"><span>Ariana Grande</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                      <li><a href="profil.html">Profil Saya</a></li>
+                      <li><a href="beranda.html">Keluar</a></li>
+                    </ul>
+                  </li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- ======= Profil ======= -->
+    <div class="page-content page-container" id="page-content">
+      <div class="padding">
+          <div class="row container d-flex justify-content-center">
+              <div class="col-xl-6 col-md-12">
+                  <div class="card user-card-full">
+                      <div class="row m-l-0 m-r-0">
+                          <div class="col-sm-8">
+                              <div class="card-block">
+                                  <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Profil Saya</h6>
+                                  <div class="row">
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">Nama Lengkap</p>
+                                          <h6 class="text-muted f-w-400">Ariana Grande</h6>
+                                      </div>
+                                      <div class="col-sm-1">
+                                        <p class="m-b-10 f-w-600">Email</p>
+                                        <h6 class="text-muted f-w-400">reservasi_yuk@gmail.com</h6>
+                                      </div>
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">Telepon</p>
+                                          <h6 class="text-muted f-w-400">081122334455</h6>
+                                      </div>
+                                      <div class="col-sm-1">
+                                        <p class="m-b-10 f-w-600">Tanggal Lahir</p>
+                                        <h6 class="text-muted f-w-400">26/06/1993</h6>
+                                      </div>
+                                      <div class="col-sm-6">
+                                            <p class="m-b-10 f-w-600">Jenis Kelamin</p>
+                                            <h6 class="text-muted f-w-400">Perempuan</h6>
+                                       </div>
+                                  </div>
+                                  <a class="nav-link scrollto" href="ubahprofil.html"><button>Ubah Profil</button></a>
+                                  <a class="nav-link scrollto" href="ubahkatasandi.html"><button>Ubah Kata Sandi</button></a>
+
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+</body>
+</html>
